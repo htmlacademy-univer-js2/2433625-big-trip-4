d8690f91f084294d278957de54597db68bed51ce
Graddery <94702063+Graddery@users.noreply.github.com>
2024-06-10 21:59:09 +0500

    fix 2

--- src/view/info-view.js
+++
@@ -1,8 +0,0 @@
-import AbstractView from '../framework/view/abstract-view';
-import { mainInfo } from '../info-template';
-
-export default class MainInfo extends AbstractView{
-  get template() {
-    return mainInfo;
-  }
-}
--- src/view/sort-view.js
+++
@@ -1,8 +0,0 @@
-import AbstractView from '../framework/view/abstract-view';
-import { createListSortElement } from '../sort-template';
-
-export default class ListSortElement extends AbstractView{
-  get template() {
-    return createListSortElement;
-  }
-}
